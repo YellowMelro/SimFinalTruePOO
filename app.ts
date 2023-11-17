3e15bb64fa00a8587f8ae95940f1777da03a334d
YellowMelro <97251484+YellowMelro@users.noreply.github.com>
2023-11-17 18:27:53 -0300

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import usuarioRoutes from './routes/usuarioRoutes';
+import leilaoRoutes from './routes/leilaoRoutes';
+import lanceRoutes from './routes/lanceRoutes';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(usuarioRoutes);
+app.use(leilaoRoutes);
+app.use(lanceRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
